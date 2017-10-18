102bb7019533be541404669e106217b4d1674ca9
Nguyen The Phuong <thephuongcqt@gmail.com>
2017-10-18 12:04:15 +0700

    Custom Menu

--- StudyQuiz/app/src/main/java/com/phuongnt/studyquiz/activity/MenuActivity.java
+++ StudyQuiz/app/src/main/java/com/phuongnt/studyquiz/activity/MenuActivity.java
@@ -1,15 +1,45 @@
 package com.phuongnt.studyquiz.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.phuongnt.studyquiz.R;
+import com.phuongnt.studyquiz.adapter.ViewPagerAdapter;
+import com.phuongnt.studyquiz.fragment.FirstFragment;
+import com.phuongnt.studyquiz.fragment.SecondFragment;
+import com.phuongnt.studyquiz.fragment.ThirdFragment;
 
 public class MenuActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setTitle("");
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FirstFragment(), "First");
+        adapter.addFragment(new SecondFragment(), "Second");
+        adapter.addFragment(new ThirdFragment(), "Third");
+        adapter.addFragment(new FirstFragment(), "fourth");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ StudyQuiz/app/src/main/java/com/phuongnt/studyquiz/fragment/SecondFragment.java
@@ -0,0 +1,35 @@
+package com.phuongnt.studyquiz.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.phuongnt.studyquiz.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+}
