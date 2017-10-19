b784580b9ace06367eb0e3fca8c7f9a47024969b
PHUCHVTSE62066\Phuc Huynh <huynhvothienphuc963@gmail.com>
2017-10-19 21:53:34 +0700

    Change layout header

--- laravel/resources/views/adminlte/header.blade.php
+++ laravel/resources/views/adminlte/header.blade.php
@@ -3,9 +3,74 @@
         <span class="logo-mini">FP</span>
         <span class="logo-lg">FitsPlus</span>
     </a>
-    <nav class="navbar navbar-static-top" role="navigation">
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-            <span class="sr-only">ナビゲーション</span>
-        </a>
+      <nav class="navbar navbar-static-top">
+      <div class="container">
+        
+
+        
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav" style="padding-left: 50px">
+            <!-- Messages: style can be found in dropdown.less-->
+             
+            <!-- User Account Menu -->
+            <li class="dropdown user user-menu">
+              <!-- Menu Toggle Button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <!-- The user image in the navbar-->
+                <img src="{{asset("/img/userIcon.jpg")}}" class="user-image" alt="User Image">
+                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                <span class="hidden-xs">   </span>
+              </a>
+              <ul class="dropdown-menu">
+                <!-- The user image in the menu -->
+                <li class="user-header">
+                  <img src="{{asset("/img/userIcon.jpg")}}" class="img-circle" alt="User Image">
+
+                  <p>
+                    Alexander Pierce - Web Developer
+                    <small>Member since Nov. 2012</small>
+                  </p>
+                </li>
+                <!-- Menu Body -->
+                <!-- <li class="user-body">
+                  <div class="row">
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Followers</a>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Sales</a>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Friends</a>
+                    </div>
+                  </div>
+                  <!-- /.row -->
+                </li>  
+                <!-- Menu Footer-->
+                <li class="user-footer">
+                  <div class="pull-left">
+                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  </div>
+                  <div class="pull-right">
+                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  </div>
+                </li>
+              </ul>
+            </li>
+             <li class="dropdown user user-menu">
+              <!-- Menu Toggle Button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <!-- The user image in the navbar-->
+                
+                <span class="hidden-xs">   </span>
+              </a>
+              
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
     </nav>
 </header>
---
+++ laravel/resources/views/adminlte/loginLayout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>DKM</title>
+    <link rel="stylesheet" href="{{asset("/bootstrap/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{asset("/css/AdminLTE.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/skins/skin-yellow-light.min.css")}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <script src="{{asset("/plugins/jQuery/jquery-3.1.1.min.js")}}"></script>
+    <script src="{{asset("/bootstrap/js/bootstrap.min.js")}}"></script>
+    <script src="{{asset("/js/validation.js")}}"></script>
+</head>
+<body class="hold-transition skin-yellow-light sidebar-mini">
+<div class="wrapper">
+    
+     
+
+    <div class="content-wrapper">
+
+        @yield('content')
+
+    </div>
+
+    
+</div>
+
+<script src="{{asset("/js/adminlte.min.js")}}"></script>
+</body>
+</html>
