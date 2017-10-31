6fb7444acf5cb7ebe7ebc0e5bb55289b4b60deed
PhuongNT <thephuongcqt@gmail.com>
2017-11-01 01:10:35 +0700

    [API C#] implement studied question controller

---
+++ StudyQuizAPI/StudyQuizAPI/Models/Service/StudiedQuestionRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudyQuizAPI.Models.Service
+{
+    public class StudiedQuestionRequest
+    {
+        public long UserId { get; set; }
+        public Dictionary<long, bool> Questions { get; set; }
+    }
+}
