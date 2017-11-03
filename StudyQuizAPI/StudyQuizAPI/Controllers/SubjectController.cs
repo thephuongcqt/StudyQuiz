9c47ebcc78487e3ee018ce15a5524c7c98085f11
PhuongNT <thephuongcqt@gmail.com>
2017-11-04 04:01:48 +0700

    [API C#]  create subject controller

---
+++ StudyQuizAPI/StudyQuizAPI/Controllers/SubjectController.cs
@@ -0,0 +1,30 @@
+﻿using StudyQuizAPI.Models;
+using StudyQuizAPI.Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudyQuizAPI.Controllers
+{
+    public class SubjectController : ApiController
+    {
+        [HttpGet]
+        public object FetchSubject(string offset, string number)
+        {
+            try
+            {
+                long longOffset = long.Parse(offset);
+                long longNumber = long.Parse(number);
+                var result = new SubjectDAO().FetchSubject(longOffset, longNumber);
+                return new Response(true, result);
+            }
+            catch (Exception e)
+            {
+                return new Response(false, null, e.Message);
+            }
+        }
+    }
+}
